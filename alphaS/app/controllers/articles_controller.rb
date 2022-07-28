87a9ba6e2708197271ab1cc7d1aba3d1d5ab1216
rahullourwork <rahul.lour@w3villa.com>
2022-07-28 12:29:08 +0530

    after edit

--- alphaS/app/controllers/articles_controller.rb
+++ alphaS/app/controllers/articles_controller.rb
@@ -1,16 +1,20 @@
 class ArticlesController < ApplicationController
-    def show
+    def index
         @articles = Article.all 
     end
-    def article1
-        # byebug
-        @articleNo1 = Article.find(1)
+    
+    def show
+        #byebug
+     @article = Article.find(params[:id])
     end
-    def article2
-        @articleNo2 = Article.find(2)
-    end
+ 
     def new
          @store = Article.new
+    end
+
+    def edit
+       # byebug
+        @article = Article.find(params[:id])
     end
 
     def create
@@ -19,11 +23,22 @@ class ArticlesController < ApplicationController
         # render plain: @store.inspect
         if @store.save
             flash[:notice] = "Article was created successfully."
-        redirect_to article_path(@store)
+            redirect_to article_path(@store)
         else
             render :new
-            # redirect_to 'locahost:3000/articles/new', allow_other_host: true
+         # redirect_to 'locahost:3000/articles/new', allow_other_host: true
         end
+    end
+
+    def update
+    # byebug
+    @article = Article.find(params[:id])
+    if  @article.update(params.require(:article).permit(:title, :des, :author))
+        flash[:notice] = "Article was updated successfully."
+        redirect_to articles_path
+    else
+        render :edit
+    end
     end
 
 end
--- alphaS/config/routes.rb
+++ alphaS/config/routes.rb
@@ -2,14 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#show"
-  get "/articles", to: "articles#show" 
-  get "/articles/1", to: "articles#article1" 
-  get "/articles/2", to: "articles#article2" 
+   # root "articles#show"
   
-  # post "/articles/new", to: "articles#new" 
-  #  get "/articles/new", to: "articles#new" 
-
-  get "/articles/create", to: "articles#create"  
-   resources :articles
+      resources :articles
 end
